3400f5ef7cb88246af83539de9b986453cbcaa4f
Ryan Cheung <ryanc@livefyre.com>
2014-08-29 12:31:11 -0700

    Introduce adaptive button color styling based on card background color lightness/darkness

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,6 +1,7 @@
 require.config({
   paths: {
-    text: 'lib/requirejs-text/text'
+    text: 'lib/requirejs-text/text',
+    tinycolor: 'node_modules/tinycolor2/tinycolor'
   },
   packages: [{
     name: "livefyre-theme-styler",
