7bbc10e700be5cdcac6cdcfae6ff6fe5e1659645
Ryan Cheung <ryanc@livefyre.com>
2014-08-29 12:34:55 -0700

    Make tinycolor a bower dep

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,7 +1,7 @@
 require.config({
   paths: {
     text: 'lib/requirejs-text/text',
-    tinycolor: 'node_modules/tinycolor2/tinycolor'
+    tinycolor: 'lib/tinycolor2/tinycolor'
   },
   packages: [{
     name: "livefyre-theme-styler",
