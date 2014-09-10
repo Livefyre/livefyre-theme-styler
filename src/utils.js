30df412720fd60d83857ac29586abbf5dded7850
Ryan Cheung <ryanc@livefyre.com>
2014-09-10 16:59:38 -0700

    Fix var name

--- src/utils.js
+++ src/utils.js
@@ -7,17 +7,17 @@ function interpolateButtonStyles(themeOpts) {
     var buttonTheme = {};
     var backgroundColor = themeOpts.cardBackgroundColor ? tinycolor(themeOpts.cardBackgroundColor) : tinycolor('#FFF');
     if (backgroundColor.isLight()) {
-        buttonthemeOpts.buttonTextColor = tinycolor('#000').lighten(40).toHexString();
-        buttonthemeOpts.buttonHoverBackgroundColor = backgroundColor.darken(5).toHexString();
-        buttonthemeOpts.buttonActiveBackgroundColor = backgroundColor.darken(15).toHexString();
-        buttonthemeOpts.buttonBorderColor = 'rgba(0,0,0,0.3)';
+        buttonThemeOpts.buttonTextColor = tinycolor('#000').lighten(40).toHexString();
+        buttonThemeOpts.buttonHoverBackgroundColor = backgroundColor.darken(5).toHexString();
+        buttonThemeOpts.buttonActiveBackgroundColor = backgroundColor.darken(15).toHexString();
+        buttonThemeOpts.buttonBorderColor = 'rgba(0,0,0,0.3)';
     } else if (backgroundColor.isDark()) {
-        buttonthemeOpts.buttonTextColor = tinycolor('#FFF').darken(40).toHexString();
-        buttonthemeOpts.buttonHoverBackgroundColor = backgroundColor.lighten(5).toHexString();
-        buttonthemeOpts.buttonActiveBackgroundColor = backgroundColor.lighten(15).toHexString();
-        buttonthemeOpts.buttonBorderColor = 'rgba(0,0,0,0.5)';
+        buttonThemeOpts.buttonTextColor = tinycolor('#FFF').darken(40).toHexString();
+        buttonThemeOpts.buttonHoverBackgroundColor = backgroundColor.lighten(5).toHexString();
+        buttonThemeOpts.buttonActiveBackgroundColor = backgroundColor.lighten(15).toHexString();
+        buttonThemeOpts.buttonBorderColor = 'rgba(0,0,0,0.5)';
     }
-    return _.extend(buttonTheme, themeOpts);
+    return _.extend(buttonThemeOpts, themeOpts);
 }
 
 module.exports = {
